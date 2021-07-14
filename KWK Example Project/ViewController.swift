f45b8ba0462a6ec2cc01f9433a7ac164488727d1
Janessa Ulug <janessaulug@gmail.com>
2021-07-14 10:57:31 -0700

    Initial Commit

---
+++ KWK Example Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KWK Example Project
+//
+//  Created by Janessa Ulug on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
