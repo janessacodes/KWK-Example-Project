e531f7370c3e664a7dd4eae150e64d9aabb37bb6
Janessa Ulug <janessaulug@gmail.com>
2021-07-21 11:01:18 -0700

    committed a comment

--- KWK Example Project/AppDelegate.swift
+++ KWK Example Project/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreData
 
 @main
+//jhello
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
--- KWK Example Project/ViewController.swift
+++ KWK Example Project/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
